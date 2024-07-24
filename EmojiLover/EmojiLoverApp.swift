18b6574d4c3e7aacac86062321741eecf131ed95
Hayk <drgevorgyanhayk@gmail.com>
2024-07-24 10:55:27 -0600

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Hayk Gevorgyan on 2024-07-24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
